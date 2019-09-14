42ebb69fd1017a6bc7a7583eeba8b0c4974d0af5
George Zografos <george.zografos@dazn.com>
2019-09-14 20:47:08 +0200

    Attach IAM policy to enable CloudFront to read from S3

--- website/s3.tf
+++ website/s3.tf
@@ -2,3 +2,30 @@ resource "aws_s3_bucket" "b" {
   bucket = "dazniasdozlck1923abcd"
   acl    = "private"
 }
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.b.arn}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = "${aws_s3_bucket.b.id}"
+  policy = "${data.aws_iam_policy_document.s3_policy.json}"
+}
